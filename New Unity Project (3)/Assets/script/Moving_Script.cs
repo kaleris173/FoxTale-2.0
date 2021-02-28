1e6adbbe52d7a476abede9fad1fcf0e22ef63e28
kaleris173 <bammer.joosep@gmail.com>
2021-02-28 17:26:06 +0200

    1

--- New Unity Project (3)/Assets/script/Moving_Script.cs
+++ New Unity Project (3)/Assets/script/Moving_Script.cs
@@ -23,14 +23,19 @@ public class Moving_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, whatisGround);
         rb.velocity = new Vector2(moveSpeed*Input.GetAxis("Horizontal"), rb.velocity.y);
+        if (isGrounded)
+        {
+            CandoubleJump = true;
+        }
         if (Input.GetButtonDown("Jump"))
         {
-            isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, whatisGround);
+            
             if (isGrounded)
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                CandoubleJump = true;
+                
             }
             else
             {
--- New Unity Project (3)/Assets/script/camera_controller.cs
+++ New Unity Project (3)/Assets/script/camera_controller.cs
@@ -6,15 +6,22 @@ public class camera_controller : MonoBehaviour
 {
     public Transform Target;
     public float minHeight, maxheight;
+    public Transform farBg, midBg;
+    private float lastXPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastXPos = transform.position.x; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        float amountToMoveX = transform.position.x - lastXPos;
+        farBg.position = farBg.position + new Vector3(amountToMoveX, 0f,0f);
+        midBg.position = midBg.position + new Vector3(amountToMoveX * .5f, 0f, 0f);
+        lastXPos = transform.position.x;
+
         transform.position = new Vector3(Target.position.x, Mathf.Clamp(Target.position.y,minHeight,maxheight), transform.position.z);
  
     }
